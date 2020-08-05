02228781b3781836b146bc19057a9277c1d12fca
Bhen <35108430+bhendrik@users.noreply.github.com>
2020-08-05 15:35:11 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo This script will start a sync from a library with an existing folder.
 echo Input new sync parameters
 read -p 'Library-ID: ' libraryid
 read -p 'Server-url: ' serverurl
-echo Library folder will be located in /data
+read -p 'Folder-name: ' foldername
 read -p 'Email-address: ' userid
-/sbin/setuser sfuser seaf-cli download -l "$libraryid" -s "$serverurl" -d "/data" -u "$userid"
+/sbin/setuser sfuser seaf-cli sync -l "$libraryid" -s "$serverurl" -d "/data/$foldername" -u "$userid"
 /sbin/setuser sfuser seaf-cli status
